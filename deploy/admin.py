b3c1e9e236564008b65e00fb374cca1bcf806e5f
liuyao <liuyao@ixianlai.com>
2019-05-06 16:24:43 +0800

    delete old projecr

--- deploy/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from deploy import models
-
-
-class SaltAdmin(admin.ModelAdmin):
-    list_display = ('name','create_date','colored_status','Autherized_date','memo')
-admin.site.register(models.SaltMinion,SaltAdmin)
